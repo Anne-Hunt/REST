618917a794b4e011195dee56310677824e1de4e3
Anne Hunt <acathunt@gmail.com>
2024-04-10 15:33:02 -0600

    create mission working

--- src/controllers/MissionsController.js
+++ src/controllers/MissionsController.js
@@ -6,12 +6,23 @@ export class MissionsController extends BaseController {
         super('/api/missions')
         this.router
             .get('', this.getMissions)
+            .post('', this.createMission)
     }
 
     async getMissions(request, response, next) {
         try {
             const missions = await missionService.getMissions()
             response.send(missions)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async createMission(request, response, next) {
+        try {
+            const missiondata = request.body
+            const missionAnew = await missionService.createMission(missiondata)
+            response.send(missionAnew)
         } catch (error) {
             next(error)
         }
--- src/services/MissionsService.js
+++ src/services/MissionsService.js
@@ -6,6 +6,13 @@ class MissionsService {
         const missions = await dbContext.Mission.find().populate('rat', '-name').populate('location')
         return missions
     }
+
+    async createMission(missiondata) {
+        const mission = await dbContext.Mission.create(missiondata)
+        await mission.populate('rat', '-name')
+        await mission.populate('location')
+        return mission
+    }
 }
 
 export const missionService = new MissionsService()
